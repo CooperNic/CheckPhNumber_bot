575912e2c1be52175841e788985696bf6300093f
Alexey <coosbloo@gmail.com>
2024-08-15 23:52:13 +0300

    Ключ API бота вынесен в .env файл

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+import sqlite3 as sl
+from dotenv import load_dotenv
+
+load_dotenv()
+API_TOKEN = os.getenv("API_TOKEN")
+
+# открываем файл с базой данных
+con = sl.connect('db.sqlite/numeration_registry.db')
+cursor = con.cursor()
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello you typed START command!")
+
+@dp.message()
+async def cmd_start(message: types.Message):
+    mess = " "
+    cursor.execute('SELECT * FROM numbers WHERE fullnumber='+message.text)
+    number = cursor.fetchall()
+    print(number)
+    await message.reply(number[0][6]+"\n"+number[0][7])
+
+#
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+con.close()
