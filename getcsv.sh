7d00e88dda0bdbd2436715a0b0c614cd1ad73792
Alexey <coosbloo@gmail.com>
2024-08-21 10:51:17 +0300

    bash скрипт загрузки csv файлов реестра с сайта Минцифры

---
+++ getcsv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://opendata.digital.gov.ru/downloads/ABC-3xx.csv?1721899999426 --no-check-certificate -O ABC-3xx.csv
+wget https://opendata.digital.gov.ru/downloads/ABC-4xx.csv?1721899999426 --no-check-certificate -O ABC-4xx.csv
+wget https://opendata.digital.gov.ru/downloads/ABC-8xx.csv?1721899999426 --no-check-certificate -O ABC-8xx.csv
+wget https://opendata.digital.gov.ru/downloads/DEF-9xx.csv?1721899999426 --no-check-certificate -O DEF-9xx.csv
