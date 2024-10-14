bdfda308ef0618d1a6b54f2a08baca9333b84650
Alexey <coosbloo@gmail.com>
2024-10-14 23:17:39 +0300

    Сделано журналирование запросов в файл access.log

--- main.py
+++ main.py
@@ -5,9 +5,27 @@ from aiogram.filters.command import Command
 import sqlite3 as sl
 from dotenv import load_dotenv
 import time
+import logging
+
 
 load_dotenv()
 API_TOKEN = os.getenv("API_TOKEN")
+
+# Создаём объект логгера
+logger = logging.getLogger(__name__)
+
+# Устанавливаем уровень логирования
+logger.setLevel(logging.INFO)
+
+# Создаем обработчик для записи в файл
+file_handler = logging.FileHandler('access.log')
+
+# Форматируем вывод
+formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
+file_handler.setFormatter(formatter)
+
+# Добавляем обработчик к логгеру
+logger.addHandler(file_handler)
 
 # открываем файл с базой данных
 con = sl.connect('db.sqlite/numeration_registry.db')
@@ -36,11 +54,14 @@ async def cmd_start(message: types.Message):
     end_time = time.time()
     execution_time = end_time - start_time
     print(f"Время выполнения: {execution_time} секунд. ")
+    log_txt_end = "|"+ message.from_user.full_name + "|@"+ message.from_user.username +"|" + str(message.from_user.id)+"|" + str(execution_time) 
     if number:
         print(number)
+        logger.info(entered_phone_number + "|"+ str(number[0][2]) + "|"+ number[0][6] + "|" + number[0][7] + log_txt_end )
         await message.reply(number[0][6]+" ("+number[0][7]+")")
     else:
         print("Номер не найден.")
+        logger.info(entered_phone_number + "|Номер не найден||" + log_txt_end )
         await message.reply("Номер не найден.")
 #
 async def main():
